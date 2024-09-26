c7f1cda2a6bc57c89ed0b71bc625c0c78b4a2b43
Karansayambar <karansayambar@gmail.com>
2024-09-26 09:27:39 +0530

    code added

--- src/ecom_3Lagent_frontend/src/App.jsx
+++ src/ecom_3Lagent_frontend/src/App.jsx
@@ -1,29 +1,49 @@
-import { useState } from 'react';
-import { ecom_3Lagent_backend } from 'declarations/ecom_3Lagent_backend';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from './layouts/MainLayout';
+import SignIn from './features/Auth/SignIn';
+import SignUp from './features/Auth/SignUp';
+import CheckoutLayout from './layouts/CheckoutLayout';
+import CompleteLayout from './layouts/CompleteLayout';
+import ProductDetailsLayout from './layouts/ProductDetailsLayout';
+import CartLayout from './layouts/CartLayout';
+import ProductLayout from './layouts/ProductLayout';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  const [isAuth, setIsAuth] = useState(false);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    ecom_3Lagent_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  useEffect(() => {
+    const storedUserData = JSON.parse(localStorage.getItem("userData"));
+    if (storedUserData && storedUserData.isAuth === true) {
+      setIsAuth(true);
+    }
+  }, []);
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {isAuth ? (
+            <>
+              <Route path="/" element={<MainLayout />} />
+              <Route path="/shop" element={<ProductLayout />} />
+              <Route path="/product" element={<ProductDetailsLayout />} />
+              <Route path="/cart" element={<CartLayout />} />
+              <Route path="/check" element={<CheckoutLayout />} />
+              <Route path="/complete" element={<CompleteLayout />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<SignIn setIsAuth={setIsAuth} />} />
+              <Route path="/signup" element={<SignUp />} />
+            </>
+          )}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </main>
   );
 }
